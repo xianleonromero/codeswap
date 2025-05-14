f680f73289f3f95312e4c24ab81e5f5feda1944b
xianleon310 <xleonr23@fpcoruna.afundacion.org>
2025-05-14 19:56:26 +0200

    Restructuring

--- codeswap_backend/sessions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Session
-
-@admin.register(Session)
-class SessionAdmin(admin.ModelAdmin):
-    list_display = ('teacher', 'student', 'language', 'status', 'date_time')
-    list_filter = ('status', 'language')
-    search_fields = ('teacher__username', 'student__username')
--- codeswap_backend/skills/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ProgrammingLanguage(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    icon = models.CharField(max_length=100, blank=True, null=True)
-
-    def __str__(self):
-        return self.name
